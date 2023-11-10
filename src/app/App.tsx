0bb16a93cd63a64f334a69d4c6141de3e7a667a9
Islombek <eislombek9599@gmail.com>
2023-11-10 20:31:54 +0900

    Feat: Develop routing based settings on Papay Project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../css/App.css";
 import { Container, Stack, Box, Typography, Button } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Users from "./components/users";
-import Dishes from "./components/dishes";
+import { RestaurantPage } from "./screens/RestaurantPage";
+import { CommunityPage } from "./screens/CommunityPage";
+import { OrdersPage } from "./screens/Orderspage";
+import { MemberPage } from "./screens/MemberPage";
+import { HelpPage } from "./screens/HelpPage";
+import { HomePage } from "./screens/HomePage";
+import { LoginPage } from "./screens/LoginPage";
 
 function App() {
   return (
@@ -13,13 +18,25 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/restaurant">RestaurantPage</Link>
             </li>
             <li>
-              <Link to="/dishes">Dishes</Link>
+              <Link to="/community">CommunityPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">MemberPage</Link>
+            </li>
+            <li>
+              <Link to="/help">HelpPage</Link>
+            </li>
+            <li>
+              <Link to="/login">LoginPage</Link>
+            </li>
+            <li>
+              <Link to="/">Homepage</Link>
             </li>
           </ul>
         </nav>
@@ -27,14 +44,26 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/dishes">
-            <Dishes />
+          <Route path="/restaurant">
+            <RestaurantPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/community">
+            <CommunityPage />
+          </Route>
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <MemberPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
           </Route>
           <Route path="/">
-            <Home />
+            <HomePage />
           </Route>
         </Switch>
       </div>
