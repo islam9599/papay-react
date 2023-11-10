90ef946192d0f9580601b7f0a010d45956db8b63
Islombek <eislombek9599@gmail.com>
2023-11-10 19:04:09 +0900

    Feat: Cutomized integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "../css/App.css";
+import { Container, Stack, Box, Typography, Button } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <Stack flexDirection={"column"}>
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component={"h1"} gutterBottom>
+              Create React app on TypeScript with Redux
+            </Typography>
+          </Box>
+          <Box>
+            <RippleBadge badgeContent={4}>
+              <Button color="secondary" variant="contained">
+                Contained
+              </Button>
+            </RippleBadge>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
