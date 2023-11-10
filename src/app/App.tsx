817c960e30d646a010e4071c4740e15a5e22598f
Islombek <eislombek9599@gmail.com>
2023-11-10 19:53:05 +0900

    Feat: Checking react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,52 @@ import React from "react";
 import "../css/App.css";
 import { Container, Stack, Box, Typography, Button } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Users from "./components/users";
+import Dishes from "./components/dishes";
 
 function App() {
   return (
-    <div className="App">
-      <Container></Container>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dishes">Dishes</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dishes">
+            <Dishes />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function Home() {
+  return (
+    <Container>
+      <h2>Home</h2>;
+    </Container>
+  );
+}
---
+++ src/app/components/users.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Stack, Box, Typography, Button } from "@mui/material";
+
+function Users() {
+  return (
+    <Container>
+      <h2>Users</h2>
+    </Container>
+  );
+}
+
+export default Users;
