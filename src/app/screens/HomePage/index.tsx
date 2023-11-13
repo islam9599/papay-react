b4c1ad1d3380e5a88ec3504e4747d6404d02628c
Islombek <eislombek9599@gmail.com>
2023-11-13 15:00:47 +0900

    Feat: develop statistics component on HomePage

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import "../../../css/home.css";
 import { Container } from "@mui/material";
+import { Statistics } from "./statistics";
+import { TopRestaurants } from "./topRestaurants";
+import { BestRestaurants } from "./bestRestaurants";
+import { BestDishes } from "./bestDishes";
+import { Advertisements } from "./advertisement";
+import { Events } from "./events";
+import { Recommendations } from "./recommendations";
 
 export function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className="homepage">
+      <Statistics />
+      <TopRestaurants />
+      <BestRestaurants />
+      <BestDishes />
+      <Advertisements />
+      <Events />
+      <Recommendations />
+    </div>
+  );
 }
