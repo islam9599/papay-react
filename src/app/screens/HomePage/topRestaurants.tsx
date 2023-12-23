a970d12c9100938280b0797d57715acddd6806b2
Islombek <eislombek9599@gmail.com>
2023-12-23 12:18:58 +0900

    Feat: develop member like topRestaurants logic

--- src/app/screens/HomePage/topRestaurants.tsx
+++ src/app/screens/HomePage/topRestaurants.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Box, Container, Stack } from "@mui/material";
+import assert from "assert";
 import {
   Card,
   CardContent,
@@ -18,6 +19,11 @@ import { createSelector } from "reselect";
 import { retrieveTopRestaurants } from "./selector";
 import { Restaurant } from "../../../types/user";
 import { serverApi } from "../../../lib/config";
+import { sweetErrorHandling } from "../../../lib/sweetAlert";
+import { Definer } from "../../../lib/Definer";
+import MemberApiService from "../../apiServices/memberApiService";
+import { MemberLiken } from "../../../types/others";
+import { useHistory } from "react-router-dom";
 
 /** Redux Selector*/
 const topRestaurantRetriever = createSelector(
@@ -31,6 +37,38 @@ export function TopRestaurants() {
   /** Initialization */
   const { topRestaurants } = useSelector(topRestaurantRetriever);
   console.log("topRestaurants::::::", topRestaurants);
+  const refs: any = useRef([]);
+  const history = useHistory();
+
+  /** Handlers */
+
+  const chosenRestaurantHandler = (id: string) => {
+    history.push(`/restaurant/${id}`);
+  };
+
+  const targetLiketop = async (e: any, id: string) => {
+    try {
+      assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
+
+      const memberService = new MemberApiService(),
+        like_result: any = await memberService.memberLikeTarget({
+          like_ref_id: id,
+          group_type: "member",
+        });
+      assert.ok(like_result, Definer.general_err1);
+
+      if (like_result.like_status > 0) {
+        e.target.style.fill = "red";
+        refs.current[like_result.like_ref_id].innerHTML++;
+      } else {
+        e.target.style.fill = "white";
+        refs.current[like_result.like_ref_id].innerHTML++;
+      }
+    } catch (err: any) {
+      console.log("err: targetLiketop", err);
+      sweetErrorHandling(err).then();
+    }
+  };
   return (
     <div className="top_restaurant_frame">
       <Container>
@@ -46,6 +84,7 @@ export function TopRestaurants() {
               return (
                 <CssVarsProvider key={ele._id}>
                   <Card
+                    onClick={() => chosenRestaurantHandler(ele._id)}
                     sx={{
                       minHeight: 430,
                       minWidth: 325,
@@ -96,6 +135,7 @@ export function TopRestaurants() {
                         }}
                       >
                         <Favorite
+                          onClick={(e) => targetLiketop(e, ele._id)}
                           style={{
                             fill:
                               ele?.me_liked && ele?.me_liked[0]?.my_favorite
@@ -126,7 +166,11 @@ export function TopRestaurants() {
                           alignItems: "center",
                         }}
                       >
-                        <div>{ele.mb_likes}</div>
+                        <div
+                          ref={(element) => (refs.current[ele._id] = element)}
+                        >
+                          {ele.mb_likes}
+                        </div>
                         <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
                       </Typography>
                     </CardOverflow>
--- src/lib/Definer.ts
+++ src/lib/Definer.ts
@@ -1,4 +1,6 @@
 export const Definer = {
+  auth_err1: "att: please login first!",
+
   input_err1: "att: please fulfill all inputs!",
   input_err2: "att: err2",
 
